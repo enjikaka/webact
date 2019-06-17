2f8213a4cdf76558c2b8fade4a02d7af9cce499a
Jeremy Karlsson <karlsson@jeremy.se>
2019-06-17 20:47:08 +0200

    Add import.meta.url

--- docs/my-app/my-app.js
+++ docs/my-app/my-app.js
@@ -1,6 +1,10 @@
 import { Component, registerComponent } from 'https://unpkg.com/webact';
 
 class MyApp extends Component {
+  constructor () {
+    super(import.meta.url);
+  }
+
   static get observedAttributes() {
     return ['to-whom'];
   }
